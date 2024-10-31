62d8d1056a5be07d5f90a03f3c610c799195e55c
TheMagicalBlob <alteringorbisgames@gmail.com>
2024-10-31 12:43:08 -0700

    ver 2.60.254

--- GP4GUI/Common.cs
+++ GP4GUI/Common.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace GP4GUI {
 
--- GP4GUI/Ver.cs
+++ GP4GUI/Ver.cs
@@ -2,7 +2,7 @@
 namespace GP4GUI {
     public partial class OptionsPage {
         // Seperate File So I'm More Likely To Open It And Update The Version Number. I Am Lazy
-        public const string Version = "ver 2.60.253 ";
+        public const string Version = "ver 2.60.254 ";
 
 
         #if DEBUG
