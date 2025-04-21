f07c299f9d0cfd72c747e3c419b8f93aa2ea006c
TheMagicalBlob <alteringorbisgames@gmail.com>
2025-04-20 22:56:24 -0700

    Ver.2.73.391 [libgp4]: missed a leftover relative path in the default blacklist.

--- GP4GUI/Ver.cs
+++ GP4GUI/Ver.cs
@@ -11,7 +11,7 @@ using static GP4GUI.Common;
 namespace GP4GUI {
     public partial class OptionsPage
     {
-        public const string Version = "2.73.389 "; // Easier to see, more likely to remember to update
+        public const string Version = "2.73.391 "; // Easier to see, more likely to remember to update
     }
 
     
